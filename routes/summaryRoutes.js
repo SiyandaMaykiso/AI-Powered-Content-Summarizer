a1edc46f0213d4ed5f3105ad8e41b8fcf24ffa4b
Siyanda Mayekiso <s.mayekiso@gmail.com>
2024-12-03 12:39:12 -0600

    Update routes

--- routes/summaryRoutes.js
+++ routes/summaryRoutes.js
@@ -2,8 +2,12 @@ const express = require('express');
 const router = express.Router();
 const summaryController = require('../controllers/summaryController');
 const authMiddleware = require('../middlewares/authMiddleware');
+const upload = require('../middlewares/fileUploadMiddleware'); // Import file upload middleware
 
-// Route to summarize content
+// Route to summarize text input
 router.post('/', authMiddleware, summaryController.summarizeContent);
+
+// Route to summarize content from uploaded files
+router.post('/upload', authMiddleware, upload.single('file'), summaryController.summarizeFile);
 
 module.exports = router;
