ca4dc844404d5c915a8b3a28fde60b9c43b0cffa
Siyanda Mayekiso <s.mayekiso@gmail.com>
2024-12-12 14:16:38 -0600

    Add footer adjust

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
     return (
         <footer
             style={{
-                position: 'fixed',
+                position: 'relative', // Change to relative
                 bottom: 0,
                 left: 0,
                 width: '100%',
